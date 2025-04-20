bb53d1f03f65bb43f185f321da0552992c8e8e79
Izyakowich <andreywillrock@gmail.com>
2025-04-20 11:51:38 +0300

    some fixes

--- src/pages/DetaliedPage/DetaliedPage.tsx
+++ src/pages/DetaliedPage/DetaliedPage.tsx
@@ -1,15 +1,105 @@
+// import * as React from 'react';
+// // import Button from 'react-bootstrap/Button';
+// import Header from 'components/Header';
+// import BreadCrumbs from 'components/BreadCrumbs';
+// import Image from "react-bootstrap/Image"
+// import styles from './DetaliedPage.module.scss'
+// import { useEffect } from 'react';
+// import { useParams } from 'react-router-dom';
+// import { mockProducts } from '../../../consts'
+// import {useDispatch} from "react-redux";
+// import { useProduct, useLinksMapData, setProductAction, setLinksMapDataAction } from "../../Slices/DetailedSlice"
+// import axios from 'axios';
+
+// export type ReceivedProductData = {
+//     id: number,
+//     product_name: string,
+//     product_info: string,
+//     price: number,
+//     status: string,
+//     photo: string,
+// }
+
+// const DetailedPage: React.FC = () => {
+//     const dispatch = useDispatch();
+//     const product = useProduct();
+//     const linksMap = useLinksMapData();
+
+//     const params = useParams();
+//     const id = params.id === undefined ? '' : params.id;
+
+//     const getProduct = async () => {
+//         try {
+//             const response = await axios.get(`http://127.0.0.1:8000/products/${id}/`);
+//             const jsonData = response.data;
+//             dispatch(setProductAction({
+//                 id: Number(jsonData.id),
+//                 title: jsonData.product_name,
+//                 price: jsonData.price,
+//                 info: jsonData.product_info,
+//                 src: jsonData.photo
+//             }))
+
+//             const newLinksMap = new Map<string, string>(linksMap); // Копирование старого Map
+//             newLinksMap.set(jsonData.title, '/products/' + id + '/');
+//             dispatch(setLinksMapDataAction(newLinksMap))
+//         } catch {
+//             const sub = mockProducts.find(item => item.id === Number(id));
+//             if (sub) {
+//                 dispatch(setProductAction(sub))
+//             }
+//         }
+//     };
+//     useEffect(() => {
+//         getProduct();
+
+//         return () => { // Возможно лучше обобщить для всех страниц в отдельный Slice !!!
+//             dispatch(setLinksMapDataAction(new Map<string, string>([['Блюда', '/products']])))
+//         }
+//     }, []);
+
+//     return (
+//         <div className='detailed__page'>
+//             <Header/>
+//             <div className={styles['detailed__page-wrapper']} style={{paddingTop: "90px"}}>
+//                 <BreadCrumbs/>
+                
+//                 <div className={styles['detailed__page-container']}>
+//                     <Image
+//                         className={styles['detailed__page-image']}
+//                         src={product?.src ? product?.src : "https://www.solaredge.com/us/sites/nam/files/Placeholders/Placeholder-4-3.jpg"}
+//                         rounded
+//                     />
+//                     <div className={styles['detailed__page-info']}>
+//                         <h4 className={styles['detailed__page-article']}>Цена на данное блюдо:  <strong>{product?.price}р.</strong></h4>
+//                         <div className={styles['detailed__page-description']}>
+//                             <h4 className={styles['detailed__page-article']}>Описание:</h4>
+//                             <p>{product?.info}</p>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// };
+  
+// export default DetailedPage;
+
+
 import * as React from 'react';
-// import Button from 'react-bootstrap/Button';
 import Header from 'components/Header';
 import BreadCrumbs from 'components/BreadCrumbs';
 import Image from "react-bootstrap/Image"
 import styles from './DetaliedPage.module.scss'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { mockProducts } from '../../../consts'
 import {useDispatch} from "react-redux";
 import { useProduct, useLinksMapData, setProductAction, setLinksMapDataAction } from "../../Slices/DetailedSlice"
 import axios from 'axios';
+import { toast } from 'react-toastify';
+import Button from 'react-bootstrap/Button';
+import { useIsAuth } from 'Slices/AuthSlice';
 
 export type ReceivedProductData = {
     id: number,
@@ -24,14 +114,47 @@ const DetailedPage: React.FC = () => {
     const dispatch = useDispatch();
     const product = useProduct();
     const linksMap = useLinksMapData();
-
+    const isUserAuth = useIsAuth();
     const params = useParams();
     const id = params.id === undefined ? '' : params.id;
+    const [isGenerating, setIsGenerating] = useState(false);
+    const [currentImage, setCurrentImage] = useState<string | undefined>();
+
+    const handleGenerateImage = async () => {
+        if (!isUserAuth) {
+            toast.info('Авторизуйтесь для генерации изображений');
+            return;
+        }
+
+        setIsGenerating(true);
+        try {
+            const response = await axios.post(
+                `http://localhost:8000/products/${id}/generateImage/`,
+                {},
+                { withCredentials: true }
+            );
+            
+            if (response.data.image_url) {
+                setCurrentImage(response.data.image_url);
+                dispatch(setProductAction({
+                    ...product,
+                    src: response.data.image_url
+                }));
+                toast.success('Изображение успешно сгенерировано!');
+            }
+        } catch (error) {
+            toast.error('Ошибка при генерации изображения');
+            console.error('Generation error:', error);
+        } finally {
+            setIsGenerating(false);
+        }
+    };
 
     const getProduct = async () => {
         try {
             const response = await axios.get(`http://127.0.0.1:8000/products/${id}/`);
             const jsonData = response.data;
+            setCurrentImage(jsonData.photo);
             dispatch(setProductAction({
                 id: Number(jsonData.id),
                 title: jsonData.product_name,
@@ -40,23 +163,25 @@ const DetailedPage: React.FC = () => {
                 src: jsonData.photo
             }))
 
-            const newLinksMap = new Map<string, string>(linksMap); // Копирование старого Map
-            newLinksMap.set(jsonData.title, '/products/' + id + '/');
+            const newLinksMap = new Map<string, string>(linksMap);
+            newLinksMap.set(jsonData.product_name, '/products/' + id + '/');
             dispatch(setLinksMapDataAction(newLinksMap))
         } catch {
             const sub = mockProducts.find(item => item.id === Number(id));
             if (sub) {
+                setCurrentImage(sub.src);
                 dispatch(setProductAction(sub))
             }
         }
     };
+
     useEffect(() => {
         getProduct();
 
-        return () => { // Возможно лучше обобщить для всех страниц в отдельный Slice !!!
+        return () => {
             dispatch(setLinksMapDataAction(new Map<string, string>([['Блюда', '/products']])))
         }
-    }, []);
+    }, [id]);
 
     return (
         <div className='detailed__page'>
@@ -65,16 +190,34 @@ const DetailedPage: React.FC = () => {
                 <BreadCrumbs/>
                 
                 <div className={styles['detailed__page-container']}>
-                    <Image
-                        className={styles['detailed__page-image']}
-                        src={product?.src ? product?.src : "https://www.solaredge.com/us/sites/nam/files/Placeholders/Placeholder-4-3.jpg"}
-                        rounded
-                    />
+                    <div className={styles['image-section']}>
+                        <div className={styles['image-wrapper']}>
+                            <Image
+                                className={styles['detailed__page-image']}
+                                src={currentImage || product?.src || "https://www.solaredge.com/us/sites/nam/files/Placeholders/Placeholder-4-3.jpg"}
+                                rounded
+                                fluid
+                            />
+                        </div>
+                        {isUserAuth && (
+                            <Button 
+                                variant="outline-secondary"
+                                onClick={handleGenerateImage}
+                                disabled={isGenerating}
+                                className={styles['generate-button']}
+                            >
+                                {isGenerating ? 'Генерация...' : 'Сгенерировать новое изображение'}
+                            </Button>
+                        )}
+                    </div>
+                    
                     <div className={styles['detailed__page-info']}>
-                        <h4 className={styles['detailed__page-article']}>Цена на данное блюдо:  <strong>{product?.price}р.</strong></h4>
+                        <h4 className={styles['detailed__page-article']}>
+                            Цена на данное блюдо: <strong>{product?.price}р.</strong>
+                        </h4>
                         <div className={styles['detailed__page-description']}>
                             <h4 className={styles['detailed__page-article']}>Описание:</h4>
-                            <p>{product?.info}</p>
+                            <p>{product?.info || 'Описание отсутствует'}</p>
                         </div>
                     </div>
                 </div>
--- src/pages/ProductsPage/ProductsPage.tsx
+++ src/pages/ProductsPage/ProductsPage.tsx
@@ -1,3 +1,231 @@
+// import * as React from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import axios from 'axios';
+// import Form from 'react-bootstrap/Form';
+// import Button from 'react-bootstrap/Button';
+// import Header from 'components/Header';
+// import OneCard from 'components/Card';
+// import styles from './ProductsPage.module.scss'
+// import { ChangeEvent } from 'react';
+// import ApplicationIcon from 'components/Icons/ApplicationIcon';
+// import BreadCrumbs from 'components/BreadCrumbs';
+// import Loader from 'components/Loader';
+// import { toast } from 'react-toastify';
+// import { mockProducts } from '../../../consts';
+// import {useUser, useIsAuth, setIsAuthAction, setUserAction} from "../../Slices/AuthSlice";
+// import { useIsMainPage } from 'Slices/MainSlice';
+// import {useDispatch} from "react-redux";
+// import {  useTitleValue, useProducts, usePriceValues,
+//       setTitleValueAction, setProductsAction, useIsProductsLoading, setIsMainPageAction, setIsProductsLoadingAction } from "../../Slices/MainSlice";
+
+// import { useLinksMapData, setLinksMapDataAction } from 'Slices/DetailedSlice';
+
+// import { useProductsFromApplication, setProductsFromApplicationAction, useCurrentApplicationId, setCurrentApplicationIdAction } from 'Slices/ApplicationsSlice';
+
+// export type product = {
+//     id: number,
+//     title: string,
+//     price: number,
+//     info: string,
+//     src: string,
+// }
+
+// export type ReceivedProductData = {
+//     id: number,
+//     product_name: string,
+//     product_info: string,
+//     price: number,
+//     status: string,
+//     photo: string,
+// }
+
+
+// export type ReceivedUserData = {
+//     id: number,
+//     email: string,
+//     full_name: string,
+//     phone_number: string,
+//     password: string,
+//     is_superuser: boolean,
+// }
+
+
+// const ProductsPage: React.FC = () => {
+//     const dispatch = useDispatch()
+//     const titleValue = useTitleValue();
+//     const products = useProducts();
+//     const priceValues = usePriceValues();
+//     const productsFromApplication = useProductsFromApplication();
+//     const linksMap = useLinksMapData();
+//     const isLoading = useIsProductsLoading()
+//     const isMainPage = useIsMainPage()
+//     const navigate = useNavigate()
+//     const currentApplicationId = useCurrentApplicationId()
+
+//     let user = useUser();
+//     const isUserAuth = useIsAuth();
+
+//     // const linksMap = new Map<string, string>([
+//     //     ['Блюда', '/']
+//     // ]);
+
+//     React.useEffect(() => {
+//         dispatch(setLinksMapDataAction(new Map<string, string>([
+//             ['Блюда', '/products']
+//         ])))
+
+//         getProducts();
+
+//         dispatch(setIsMainPageAction(true))
+
+//         return () => {
+//             dispatch(setIsMainPageAction(false))
+//         }
+
+//     }, [])
+
+//     const getProducts = async () => {
+//         let url = 'http://localhost:8000/products/'
+//         // if (titleValue) {
+//             url += `?title=${titleValue}`
+        
+//         try {
+//             const response = await axios(url, {
+//                 method: 'GET',
+//                 withCredentials: true 
+//             });
+//             const jsonData = response.data.products;
+//             const newArr = jsonData.map((raw: ReceivedProductData) => ({
+//                 id: raw.id,
+//                 title: raw.product_name,
+//                 price: raw.price,
+//                 info: raw.product_info,
+//                 src: raw.photo
+//             }));
+//             dispatch(setProductsAction(newArr));
+//         }
+//         catch {
+//             console.log('запрос не прошел !')
+//             if (titleValue) {
+//                 const filteredArray = mockProducts.filter(mockProduct => mockProduct.title.includes(titleValue));
+//                 dispatch(setProductsAction(filteredArray));
+//             } else if (priceValues) {
+//                 const filteredArray = mockProducts.filter(mockProduct => mockProduct.price <= priceValues[1]);
+//                 dispatch(setProductsAction(filteredArray));
+//             }
+//             else {
+//                 dispatch(setProductsAction(mockProducts));
+//             }
+//         }
+//         finally {
+//             dispatch(setIsProductsLoadingAction(false))
+//         }
+//     };
+
+//     const getProductss = async () => {
+//         try {
+//             const response = await axios(`http://localhost:8000/products/`, {
+//                 method:'GET',
+//                 withCredentials: true,
+//             })
+//             const request_id = response.data.application_id;
+//             dispatch(setCurrentApplicationIdAction(request_id));
+//         }
+//         catch (error) {
+//             console.log(error)
+//         }
+//     }
+
+//     const postProductToApplication = async (id: number) => {
+//         try {
+//             const response = await axios(`http://localhost:8000/products/${id}/post/`, {
+//                 method: 'POST',
+//                 withCredentials: true,
+//             })
+//             const addedProduct = {
+//                 id: response.data.id,
+//                 title: response.data.product_name,
+//                 price: response.data.price,
+//                 info: response.data.product_info,
+//                 src: response.data.photo
+//             }
+//             console.log(response)
+//             getProductss();
+//             dispatch(setProductsFromApplicationAction([...productsFromApplication, addedProduct]))
+//             toast.success("Блюдо добавлено в заявку!");
+//         } catch {
+//             toast.error("Блюдо уже добавлен в заявку!");
+//         }
+//     }
+
+//     dispatch(setIsProductsLoadingAction(true))
+//     const handleSearchButtonClick = () => {
+//         getProducts();
+//     }
+
+//     const handleTitleValueChange = (event: ChangeEvent<HTMLInputElement>) => {
+//         dispatch(setTitleValueAction(event.target.value));
+//     };
+
+//     const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//         event.preventDefault();
+//     };
+
+//     const handleClick = (id: number) => {
+//         navigate(`/applications/${id}/`, { state: { flag: false } });
+//     };
+
+
+//     return (
+//         <div className={styles['main__page']}>
+//             <Header/>
+//             <div className={styles['main__page-wrapper']}>
+//             <BreadCrumbs/>
+
+//                 {isUserAuth && !user.isSuperuser &&
+//                         <div className={styles['application__icon-wrapper']}>
+//                             {isMainPage &&
+//                                 // <ApplicationIcon onClick={() => navigate(`/applications/${currentApplicationId}/`)}/>
+//                                 <ApplicationIcon onClick={() => {currentApplicationId !== null && handleClick(currentApplicationId)}}/>
+//                             }
+//                         </div>
+//                     }
+//                 <Form className={styles['form']} onSubmit={handleFormSubmit}>
+//                     <div className={styles.form__wrapper}>
+//                         {/* <Form.Group controlId="search__sub.input__sub"> */}
+//                         <div className={styles['form__input-block']}>
+//                             <Form.Control className={styles.form__input} value={titleValue} onChange={handleTitleValueChange} type="text" placeholder="Введите название блюда..." />
+//                             <Button className={styles.form__button} onClick={() => handleSearchButtonClick()}>Найти</Button>
+//                         </div>
+//                         {/* </Form.Group> */}
+//                         <div className={styles['form__dropdown-wrapper']}>
+                            
+//                         </div>
+//                         <Button className={styles['form__mobile-button']} onClick={() => handleSearchButtonClick()}>Найти</Button>
+//                     </div>
+//                 </Form>
+
+//                 <div className={styles["main__page-cards"]}>
+//                     {products.map((product: product) => (
+//                         <OneCard id={product.id} src={product.src} onButtonClick={() => postProductToApplication(product.id)} title={product.title} price={Number(product.price)}></OneCard>
+//                     ))}
+//                 </div>
+
+//                 {isLoading ? <div className={styles.loader__wrapper}>
+//                     <Loader className={styles.loader} size='l' />
+//                  </div>
+//                  : <div className={styles["main__page-cards"]}>
+                    
+//                     </div>
+//                  }
+//             </div>
+//         </div>
+//     )
+// };
+  
+// export default ProductsPage;
+
+
 import * as React from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
@@ -6,7 +234,7 @@ import Button from 'react-bootstrap/Button';
 import Header from 'components/Header';
 import OneCard from 'components/Card';
 import styles from './ProductsPage.module.scss'
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useState } from 'react';
 import ApplicationIcon from 'components/Icons/ApplicationIcon';
 import BreadCrumbs from 'components/BreadCrumbs';
 import Loader from 'components/Loader';
@@ -17,9 +245,7 @@ import { useIsMainPage } from 'Slices/MainSlice';
 import {useDispatch} from "react-redux";
 import {  useTitleValue, useProducts, usePriceValues,
       setTitleValueAction, setProductsAction, useIsProductsLoading, setIsMainPageAction, setIsProductsLoadingAction } from "../../Slices/MainSlice";
-
 import { useLinksMapData, setLinksMapDataAction } from 'Slices/DetailedSlice';
-
 import { useProductsFromApplication, setProductsFromApplicationAction, useCurrentApplicationId, setCurrentApplicationIdAction } from 'Slices/ApplicationsSlice';
 
 export type product = {
@@ -39,17 +265,6 @@ export type ReceivedProductData = {
     photo: string,
 }
 
-
-export type ReceivedUserData = {
-    id: number,
-    email: string,
-    full_name: string,
-    phone_number: string,
-    password: string,
-    is_superuser: boolean,
-}
-
-
 const ProductsPage: React.FC = () => {
     const dispatch = useDispatch()
     const titleValue = useTitleValue();
@@ -61,33 +276,55 @@ const ProductsPage: React.FC = () => {
     const isMainPage = useIsMainPage()
     const navigate = useNavigate()
     const currentApplicationId = useCurrentApplicationId()
-
-    let user = useUser();
+    const user = useUser();
     const isUserAuth = useIsAuth();
-
-    // const linksMap = new Map<string, string>([
-    //     ['Блюда', '/']
-    // ]);
+    const [generatingProducts, setGeneratingProducts] = useState<number[]>([]);
 
     React.useEffect(() => {
         dispatch(setLinksMapDataAction(new Map<string, string>([
             ['Блюда', '/products']
         ])))
-
         getProducts();
-
         dispatch(setIsMainPageAction(true))
-
         return () => {
             dispatch(setIsMainPageAction(false))
         }
-
     }, [])
+
+    const handleGenerateImage = async (productId: number) => {
+        if (!isUserAuth) {
+            toast.info('Авторизуйтесь для генерации изображений');
+            return;
+        }
+        
+        setGeneratingProducts(prev => [...prev, productId]);
+        try {
+            const response = await axios.post(
+                `http://localhost:8000/products/${productId}/generateImage/`,
+                {},
+                { withCredentials: true }
+            );
+            
+            if (response.data.image_url) {
+                const updatedProducts = products.map(p => 
+                    p.id === productId ? { ...p, src: response.data.image_url } : p
+                );
+                dispatch(setProductsAction(updatedProducts));
+                toast.success('Изображение успешно сгенерировано!');
+            }
+        } catch (error) {
+            toast.error("Ошибка генерации изображения");
+            console.error('Generation error:', error);
+        } finally {
+            setGeneratingProducts(prev => prev.filter(id => id !== productId));
+        }
+    };
 
     const getProducts = async () => {
         let url = 'http://localhost:8000/products/'
-        // if (titleValue) {
+        if (titleValue) {
             url += `?title=${titleValue}`
+        }
         
         try {
             const response = await axios(url, {
@@ -149,7 +386,6 @@ const ProductsPage: React.FC = () => {
                 info: response.data.product_info,
                 src: response.data.photo
             }
-            console.log(response)
             getProductss();
             dispatch(setProductsFromApplicationAction([...productsFromApplication, addedProduct]))
             toast.success("Блюдо добавлено в заявку!");
@@ -158,8 +394,8 @@ const ProductsPage: React.FC = () => {
         }
     }
 
-    dispatch(setIsProductsLoadingAction(true))
     const handleSearchButtonClick = () => {
+        dispatch(setIsProductsLoadingAction(true))
         getProducts();
     }
 
@@ -175,49 +411,67 @@ const ProductsPage: React.FC = () => {
         navigate(`/applications/${id}/`, { state: { flag: false } });
     };
 
-
     return (
         <div className={styles['main__page']}>
             <Header/>
             <div className={styles['main__page-wrapper']}>
-            <BreadCrumbs/>
+                <BreadCrumbs/>
 
                 {isUserAuth && !user.isSuperuser &&
-                        <div className={styles['application__icon-wrapper']}>
-                            {isMainPage &&
-                                // <ApplicationIcon onClick={() => navigate(`/applications/${currentApplicationId}/`)}/>
-                                <ApplicationIcon onClick={() => {currentApplicationId !== null && handleClick(currentApplicationId)}}/>
-                            }
-                        </div>
-                    }
+                    <div className={styles['application__icon-wrapper']}>
+                        {isMainPage &&
+                            <ApplicationIcon onClick={() => {currentApplicationId !== null && handleClick(currentApplicationId)}}/>
+                        }
+                    </div>
+                }
+                
                 <Form className={styles['form']} onSubmit={handleFormSubmit}>
                     <div className={styles.form__wrapper}>
-                        {/* <Form.Group controlId="search__sub.input__sub"> */}
                         <div className={styles['form__input-block']}>
-                            <Form.Control className={styles.form__input} value={titleValue} onChange={handleTitleValueChange} type="text" placeholder="Введите название блюда..." />
-                            <Button className={styles.form__button} onClick={() => handleSearchButtonClick()}>Найти</Button>
+                            <Form.Control 
+                                className={styles.form__input} 
+                                value={titleValue} 
+                                onChange={handleTitleValueChange} 
+                                type="text" 
+                                placeholder="Введите название блюда..." 
+                            />
+                            <Button 
+                                className={styles.form__button} 
+                                onClick={() => handleSearchButtonClick()}
+                            >
+                                Найти
+                            </Button>
                         </div>
-                        {/* </Form.Group> */}
-                        <div className={styles['form__dropdown-wrapper']}>
-                            
-                        </div>
-                        <Button className={styles['form__mobile-button']} onClick={() => handleSearchButtonClick()}>Найти</Button>
+                        <Button 
+                            className={styles['form__mobile-button']} 
+                            onClick={() => handleSearchButtonClick()}
+                        >
+                            Найти
+                        </Button>
                     </div>
                 </Form>
 
                 <div className={styles["main__page-cards"]}>
                     {products.map((product: product) => (
-                        <OneCard id={product.id} src={product.src} onButtonClick={() => postProductToApplication(product.id)} title={product.title} price={Number(product.price)}></OneCard>
+                        <OneCard 
+                            key={product.id}
+                            id={product.id} 
+                            src={product.src} 
+                            onButtonClick={() => postProductToApplication(product.id)}
+                            onGenerateClick={() => handleGenerateImage(product.id)}
+                            isGenerating={generatingProducts.includes(product.id)}
+                            title={product.title} 
+                            price={Number(product.price)}
+                            canGenerate={isUserAuth}
+                        />
                     ))}
                 </div>
 
-                {isLoading ? <div className={styles.loader__wrapper}>
-                    <Loader className={styles.loader} size='l' />
-                 </div>
-                 : <div className={styles["main__page-cards"]}>
-                    
+                {isLoading && 
+                    <div className={styles.loader__wrapper}>
+                        <Loader className={styles.loader} size='l' />
                     </div>
-                 }
+                }
             </div>
         </div>
     )
